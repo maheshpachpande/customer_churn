0370d9a15a75082905dc2f869432e06ee806e099
maheshpachpande <pachpandemahesh300@gmail.com>
2024-03-13 09:06:52 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,30 @@
+# It is providing the necessary details for packaging, distributing, and installing your code!
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "customer_churn" # repository name of github
+AUTHOR_USER_NAME = "maheshpachpande"
+SRC_REPO = "customer_churn"  # project name src/customer_churn
+AUTHOR_EMAIL = "pachpandemahesh3000@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
